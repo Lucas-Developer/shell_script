485d80621dc0a636076b022e40f036c32df1626f
Wellington Geek <wellingtongeek@hotmail.com>
2016-04-06 20:00:37 -0300

    Alterações do arquivo. Adiconando um sleep

--- permissao.sh
+++ permissao.sh
@@ -6,5 +6,9 @@
 # email: wellingtongeek@gmail.com, contato@wellingtongeek.com
 # Para execultar o script basta localizar a pasta e digitar chmod +x permissao.sh
 
+echo "Mudando a permissão da pasta htdocs lampp"
 sudo chmod -R 777 /opt/lampp/htdocs
+echo ""
+echo "Finalizando.."
 sudo chown -R www-data:www-data  /opt/lampp/htdocs
+sleep 3
